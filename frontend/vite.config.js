b024e1a156772e3a1c9d8a8f7d57dfdc18f074b6
kushalbhuma <bhumakushal@example.com>
2025-04-16 21:26:21 +0530

    Fix and clean up vite config and update package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,17 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      crypto: 'crypto-browserify',  // Resolves `crypto` to `crypto-browserify`
+      '@': path.resolve(__dirname, './src'), // Allows shorthand import for src
     },
   },
-})
+  optimizeDeps: {
+    include: ['crypto-browserify'],  // Ensures polyfill is included in dependencies
+  },
+});
