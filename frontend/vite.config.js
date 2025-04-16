75a7aadd01b77949b7284350d874c4105aad6555
kushalbhuma <bhumakushal@example.com>
2025-04-17 00:04:54 +0530

    chore: add Node polyfills for crypto, buffer, process

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,30 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      crypto: 'crypto-browserify',  // Resolves `crypto` to `crypto-browserify`
-      '@': path.resolve(__dirname, './src'), // Allows shorthand import for src
+      crypto: 'crypto-browserify',
+      '@': path.resolve(__dirname, './src'),
     },
   },
   optimizeDeps: {
-    include: ['crypto-browserify'],  // Ensures polyfill is included in dependencies
+    include: ['crypto-browserify'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
   },
 });
